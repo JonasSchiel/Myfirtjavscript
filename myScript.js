45ffe051cab3d1024ac54297eb9a2233451d45c7
Jonas <Jonas.s874@slcstudents.org>
2019-01-16 13:48:44 -0700

    stuff happened

---
+++ myScript.js
@@ -0,0 +1,18 @@
+//This function changes the text in the demo paragraph
+
+function myFunction () {
+    document.getElementById('demo').innerHTML =
+    "paragraph Changed";
+
+}
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = 
+    z;
+    
+}
